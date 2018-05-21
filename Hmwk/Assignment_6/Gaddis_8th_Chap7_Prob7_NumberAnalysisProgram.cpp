eb5e60d2311bf9bc1f2860dc0fdc664097c96889
it2676965 <itorres32@student.rccd.edu>
2018-05-20 23:33:57 -0700

    Signed-off-by: it2676965 <itorres32@student.rccd.edu>

---
+++ Hmwk/Assignment_6/Gaddis_8th_Chap7_Prob7_NumberAnalysisProgram.cpp
@@ -0,0 +1,48 @@
+/*
+ * Write a program that asks the user for a file name. Assume the file contains 
+ * a series of numbers, each written on a separate line. The program should read
+ * the contents of the file into an array and then display the following data:
+ * The lowest number in the array
+ * The highest number in the array
+ * The total of the numbers in the array
+ * The average of the numbers in the array
+ */
+
+/* 
+ * File:   Gaddis_8th_Chap7_Prob7_NumberAnalysisProgram.cpp
+ * Author: Izeth Torres 
+ *
+ * Created on May 20, 2018, 10:48 PM
+ */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+using namespace std;
+void lowest;
+void higest;
+void total;
+void average;
+
+int main() 
+{
+    const int ARRAY =12;    //Array size
+    int number[ARRAY];      //Array with 12 numbers
+    int count = 0;          //counter loop
+    string file;            //To hold user file name
+    
+    //Ask user for file name
+    cout << "Please enter the name of the file you want to open.\n";
+    cin >> file;
+    
+    //Open file
+    inputFile.open(file);
+    
+    if 
+    
+    
+    return 0;
+}
+
